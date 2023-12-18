a599c11ebbe1927e6b5e2fd59159a052fda2ac19
ivo garofalo <ivo.garofalo@gmail.com>
2023-12-18 18:05:34 +0100

    Add OptimizedContract and OptimizedContract test

---
+++ contracts/OptimizedContract.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.15;
+
+contract OptimizedContract {
+
+    /// @notice Code1 is emitted when the distribute function is called before the distributeTime
+    /// @dev Read documentation section {error_codes} for detailed description of all error codes.
+    error Code1();
+
+    constructor() payable {}
+
+    function distribute(address[] calldata recipients) external {
+        
+        uint256 _bal = address(this).balance;
+
+        uint256 _amount = (_bal / recipients.length);
+
+        for(uint256 i = 0; i < recipients.length; i++) {
+            (bool success, ) = recipients[i].call{value : _amount}("");
+            if(!success) revert Code1();
+        }
+
+    }
+}
